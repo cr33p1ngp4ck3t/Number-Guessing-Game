5784565ec82bd7e2a44c9f66a3b73dddd3b6f8a3
AnasKhan44 <anaskhan.ak2005@gmail.com>
2024-04-06 16:25:23 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const guess = await inquirer.prompt([{ message: "Enter a number Between 1-10: ", type: "number", name: "userInp" }]);
+const randInt = (Math.floor(Math.random() * 10) + 1);
+if (guess.userInp === randInt) {
+    console.log("==> You Guessed The Correct Number!");
+}
+else
+    console.log(`==> Nope. The Correct Number is: ${randInt}`);
