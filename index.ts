fedce2e2ff0374f64fad8da6913a4060ff6b32d6
AnasKhan44 <anaskhan.ak2005@gmail.com>
2024-04-08 20:57:02 +0500

    Tweaked Console

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import chalk from "chalk"
 console.log(chalk.bold(`You got ${chalk.bgRed(5)} Attempts\n`))
 const randInt:number = (Math.floor(Math.random()*10)+1)
 for(let i=5;i>=1;i--){
-    const guess = await inquirer.prompt([{message:"Enter a number Between 1-10: ",type:"number",name:"userInp"}])
+    const guess = await inquirer.prompt([{message:"Guess a Number Between 1-10: ",type:"number",name:"userInp"}])
     if (guess.userInp !== randInt){
             console.log(`==> ${chalk.bgBlue('Nope')} \n\tYou got ${chalk.bgRed(i-1+' Attempts')} left!!\n`)
 
